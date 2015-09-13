78d20b022209ae70b9f0e7e6f0b076a9f64280c7
hugotorres <hugotorreslinares@gmail.com>
2015-09-12 22:40:22 -0500

    initial

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name truequeStamplayApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the truequeStamplayApp
+ */
+angular.module('truequeStamplayApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name truequeStamplayApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the truequeStamplayApp
+ */
+angular.module('truequeStamplayApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
